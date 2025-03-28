47bea7329789114ae6af47d4b9345f49ae27739d
Anes Khelif <82465490+AnesDev@users.noreply.github.com>
2025-03-28 12:38:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,27 +6,40 @@ import numpy as np
 from PIL import Image
 from streamlit_drawable_canvas import st_canvas
 
-
 model = CNN()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model.load_state_dict(torch.load("emnist_model.pt", map_location=device))
+model.load_state_dict(torch.load("src/emnist_model.pt", map_location=device))
 model.eval()
 
-st.subheader("Test Sample from Dataset")
-if st.button("Test Exported Sample"):
-    sample_img = Image.open("test_sample.png").convert("L")
-    sample_img = sample_img.resize((28, 28))  # Resize to (28, 28)
+st.subheader("Draw a character and classify it")
 
-    # Preprocess the image:
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),  
-        transforms.Normalize((0.5,), (0.5,))
-    ])
-    img_tensor = transform_test(sample_img).unsqueeze(0)  # Shape: (1, 1, 28, 28)
 
-    with torch.no_grad():
-        output = model(img_tensor)
-        prediction = torch.argmax(output, dim=1).item()
+canvas_result = st_canvas(
+    fill_color="black",
+    stroke_width=15,
+    stroke_color="white",
+    background_color="black",
+    width=280,
+    height=280,
+    drawing_mode="freedraw",
+    key="canvas"
+)
 
-    st.image(sample_img, caption="Test Sample (28x28)", width=100)
-    st.write(f"### Model Prediction: **{prediction}**")
+if st.button("Classify Drawing"):
+    if canvas_result.image_data is not None:
+        img = Image.fromarray((canvas_result.image_data[:, :, 0]).astype(np.uint8))
+        img = img.resize((28, 28))
+        img = Image.eval(img, lambda x: 255 - x)
+        # Preprocess the image
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        img_tensor = transform(img).unsqueeze(0)
+
+        with torch.no_grad():
+            output = model(img_tensor)
+            prediction = torch.argmax(output, dim=1).item()
+
+        st.image(img, caption="Processed Input (28x28)", width=100)
+        st.write(f"### Model Prediction: **{prediction}**")
