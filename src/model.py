641eedd111f0e7382d249db576d45187f72c0832
Anes Khelif <82465490+AnesDev@users.noreply.github.com>
2025-04-17 19:44:27 +0100

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,28 +1,42 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
 
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3)
-        self.conv3 = nn.Conv2d(32, 64, kernel_size=3)
 
-        self.fc1 = nn.Linear(64 * 22 * 22, 128)
-        self.fc2 = nn.Linear(128, 47)
-        
-        self.dropout = nn.Dropout(0.6)
+        self.block1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.block2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+        self.block3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.fc1 = nn.Linear(64 * 35 * 35, 64)
+        self.fc2 = nn.Linear(64, 62)
+
+        self.dropout = nn.Dropout(0.3)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        
         x = torch.flatten(x, 1)
 
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)
+        x = self.dropout(self.fc1(x))
         x = self.fc2(x)
         return x
